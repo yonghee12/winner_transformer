737baf943caf37a4b1a3f5496815892ce4fadd7f
yonghee12 <suncar1000@gmail.com>
2020-10-28 17:21:20 +0900

    add pickle saving

--- transformer_from_torch.py
+++ transformer_from_torch.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pickle
 from time import perf_counter
@@ -10,36 +11,61 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import Adam, AdamW, Adagrad, SGD
 from torch.utils.data import DataLoader, TensorDataset
+from progress_timer import Timer
 
 from winner_transformer_model import InputBlock, WinnerTransformer, OutputBlock
 
+FROM_PICKLE = True
 SPECIAL_TOK = 0
 
-X = pd.read_pickle('./X.pkl')
-y = pd.read_pickle("./y.pkl")
+if not FROM_PICKLE:
 
-# X = pd.read_pickle('./X_large.pkl')
-# y = pd.read_pickle("./y_large.pkl")
-# X = pickle.load('./X_large.pkl')
-# y = pickle.load('./y_large.pkl')
+    X = pd.read_pickle('./data/X_1027.pkl')
+    y = pd.read_pickle("./data/y_1027.pkl")
 
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=13)
-
-
-def enc_dec_split(data):
-    enc, dec = [], []
-    for line in data:
-        enc.append([SPECIAL_TOK] + list(line[:5]))
-        dec.append([SPECIAL_TOK] + list(line[:5]))
-    return np.array(enc), np.array(dec)
+    # X = pd.read_pickle('./X_large.pkl')
+    # y = pd.read_pickle("./y_large.pkl")
+    # X = pickle.load('./X_large.pkl')
+    # y = pickle.load('./y_large.pkl')
 
 
-X_train_enc, X_train_dec = enc_dec_split(X_train)
-X_test_enc, X_test_dec = enc_dec_split(X_test)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=13)
 
-# batch_size = len(X_train_enc) // 150
-# batch_size = len(X_train_enc) // 70
+
+    def enc_dec_split(data):
+        enc, dec = [], []
+        timer = Timer(len(data))
+        for idx, line in enumerate(data):
+            timer.time_check(idx)
+            enc.append(line[:5])
+            dec.append([SPECIAL_TOK] + line[5:])
+
+            if idx == 0:
+                print("original:", line)
+                print('enc:', enc[0])
+                print('dec:', dec[0])
+        return np.array(enc), np.array(dec)
+
+
+    X_train_enc, X_train_dec = enc_dec_split(X_train)
+    X_test_enc, X_test_dec = enc_dec_split(X_test)
+
+    # batch_size = len(X_train_enc) // 150
+    # batch_size = len(X_train_enc) // 70
+    fnames = ['X_train_enc', 'X_train_dec', 'y_train', 'X_test_enc', 'X_test_dec', 'y_test']
+    for fname in fnames:
+        print(fname)
+        path = os.path.join('data', fname + '.pkl')
+        pd.to_pickle(eval(fname), path)
+
+else:
+    X_train_enc = pd.read_pickle(os.path.join('data', 'X_train_enc.pkl'))
+    X_train_dec = pd.read_pickle(os.path.join('data', 'X_train_dec.pkl'))
+    y_train = pd.read_pickle(os.path.join('data', 'y_train.pkl'))
+    X_test_enc = pd.read_pickle(os.path.join('data', 'X_test_enc.pkl'))
+    X_test_dec = pd.read_pickle(os.path.join('data', 'X_test_dec.pkl'))
+    y_test = pd.read_pickle(os.path.join('data', 'y_test.pkl'))
+
 batch_size = 5000
 
 X_train_enc = torch.tensor(X_train_enc, device='cpu')
@@ -56,18 +82,17 @@ train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
 test_ds = TensorDataset(X_test_enc, X_test_dec, y_test)
 test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=True)
 
-
 hidden_dim = 256
 n_head = 8
 n_layers = 6
 
-inputblock = InputBlock(n_champs=151, hidden_dim=hidden_dim, n_positions=6)
-outputblock = OutputBlock(hidden_size=hidden_dim)
+inputblock = InputBlock(n_champs=151, hidden_dim=hidden_dim, n_positions=6, dropout=0.1)
+outputblock = OutputBlock(hidden_size=hidden_dim, dropout=0.1)
 
 transformer = nn.Transformer(d_model=hidden_dim, nhead=n_head, num_decoder_layers=n_layers, num_encoder_layers=n_layers,
                              dim_feedforward=hidden_dim * 4, dropout=0.1, activation='gelu')
 
-model = WinnerTransformer(inputblock, transformer, outputblock)
+model = WinnerTransformer(inputblock, transformer, outputblock, pooling_method='first')
 
 device = torch.device('cuda:0')
 model.to(device)
@@ -77,7 +102,6 @@ OPTIMIZER = 'AdamW'
 optimizers = {'Adam': Adam, "AdamW": AdamW, "Adagrad": Adagrad, "SGD": SGD}
 optimizer = optimizers[OPTIMIZER]
 optimizer = optimizer(model.parameters())
-
 
 # train codes from here
 total_epochs = 0
@@ -140,7 +164,6 @@ for epoch in range(n_epochs):
     test_acc = epoch_test_n_truth / len(y_test)
     print(f"epoch: {total_epochs}, loss: {loss_s}, acc: {acc:.3f}, test_acc: {test_acc:.3f}")
     total_epochs += 1
-
 
 print('asdf')
 print('hasdfla')
